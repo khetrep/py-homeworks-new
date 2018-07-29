4aaa6ba2ec7dd299a90e91f5964d3e6e65c025a2
fodev <yurisr@mail.ru>
2018-07-30 00:48:25 +0500

    Домашнее задание к лекции 3.2 «Работа с библиотекой requests, http-запросы»

--- 3.2-requests/practice.py
+++ 3.2-requests/practice.py
@@ -12,41 +12,63 @@
 """
 import requests
 import os
+from http import HTTPStatus
+
+REQUEST_TIMEOUT = 60
 
 
-def translate_file(file, file_out, lang_from, lang_to='ru'):
-    print('start translate \"{}\"'.format(file))
+def list_txt_files_in_dir(target_dir):
+    files = []
+    for file in os.listdir(target_dir):
+        if file.endswith(".txt"):
+            files.append(os.path.join(target_dir, file))
+    return files
+
+
+def ya_translate_file(text, lang_from, lang_to='ru'):
     url = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
     key = 'trnsl.1.1.20180727T083645Z.7d690c2e7b45eee9.f4f26fc075811e2f49501003eb233303b746c747'
-
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    file_abs = os.path.join(current_dir, file)
-    file_out_abs = os.path.join(current_dir, file_out)
-
-    with open(file_abs) as f:
-        text = f.read()
-
     params = {
         'key': key,
         'lang': lang_from + '-' + lang_to,
         'text': text,
     }
+    response = requests.get(url, params=params, timeout=REQUEST_TIMEOUT)
+    if response.status_code == HTTPStatus.OK:
+        translated_text = ' '.join(response.json().get('text', []))
+        return translated_text
+    else:
+        print('Failed translate: {}'.format(response.text))
 
-    timeout = 60
-    response = requests.get(url, params=params, timeout=timeout)
-    # print(response.text)
-    translated_text = ' '.join(response.json().get('text', []))
-    with open(file_out_abs, 'w') as f:
-        f.write(translated_text)
-    print('complete translate \"{}\", write to \"{}\"'.format(file, file_out))
-    return translated_text
+
+def read_file(file):
+    with open(file) as f:
+        text = f.read()
+    return text
+
+
+def write_file(file, text):
+    with open(file, 'w') as f:
+        f.write(text)
+
+
+def translate_file(file, file_out, lang, lang_to):
+    print(f'start translate \"{file}\"')
+    text = read_file(file)
+    translated_text = ya_translate_file(text, lang, lang_to)
+    if translated_text:
+        write_file(file_out, translated_text)
+        print(f'complete translate \"{file}\", write to \"{file_out}\"')
 
 
 def main():
     lang_to = 'ru'
-    for file in ['DE.txt', 'ES.txt', 'FR.txt']:
-        lang = file.lower()[:2]
-        translate_file(file, '{}.{}.txt'.format(lang.upper(), lang_to), lang, lang_to)
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    files = list_txt_files_in_dir(current_dir)
+    for file in files:
+        lang = os.path.basename(file).lower()[:2]
+        file_out = os.path.join(os.path.dirname(file), '{}.{}.txt'.format(lang.upper(), lang_to))
+        translate_file(file, file_out, lang, lang_to)
 
 
 if __name__ == "__main__":
